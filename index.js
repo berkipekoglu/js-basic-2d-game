78fd5d0bdaa201a8153e5467d5bed3432f7ba33e
berkipekoglu <berkipekoglu@gmail.com>
2022-04-01 17:04:19 +0300

    project setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+class Player {
+  constructor(){
+    this.position = {
+      x: 100,
+      y: 100
+    };
+
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw(){
+    
+  }
+}
